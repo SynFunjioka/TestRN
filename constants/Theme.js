18aa7a5e14ce2c9a7fcc0fe9c5b89897135145be
SynFunjioka <alanff2800@gmail.com>
2022-01-10 20:48:05 -0600

    first commit

---
+++ constants/Theme.js
@@ -0,0 +1,23 @@
+export default{
+    COLORS: {
+        PRIMARY: '#DBB74F',
+        SECONDARY: '#4F68DB',
+        ERROR: '#DB445E',
+        SUCCESS: '#65DB7B',
+        WARNING: '#E89D48',
+        
+        ACTIVE: '#8F7425',
+        DISABLED: '#757459',
+        OTHER:{
+          GOOGLEBTN: '#DD4B39'
+        }
+      },
+      SIZES: {
+        BLOCK_SHADOW_RADIUS: 2,
+        TEXT: {
+          TITLE: 25,
+          SUBTITLE:20,
+          NORMAL:15
+        }
+      }
+};
